4a2afdeb85afae0c791e541bba10d067bdf8df18
Azgardians <b517007@iiit-bh.ac.in>
2020-04-20 07:28:56 +0530

    The api for the currency conversion is added. The style is also updated. The folder structure is managed.

---
+++ js/navbar.js
@@ -0,0 +1,24 @@
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.padding = "30px 10px";
+    document.getElementById("logo").style.fontSize = "25px";
+  } else {
+    document.getElementById("navbar").style.padding = "50px 10px";
+    document.getElementById("logo").style.fontSize = "35px";
+  }
+  if (document.body.scrollTop > 280 || document.documentElement.scrollTop > 280){
+    document.getElementById("navbar").style.backgroundColor = "black";
+    document.getElementById("logo").style.backgroundColor = "#aff2ff";
+  }else{
+    document.getElementById("navbar").style.backgroundColor = "";
+    document.getElementById("logo").style.backgroundColor = "";
+  }
+
+}
+// for Converting the given denomination.
+function priceConverter(valNum) {
+  document.getElementById("outputPrice").innerHTML = valNum * 76.50;
+}
